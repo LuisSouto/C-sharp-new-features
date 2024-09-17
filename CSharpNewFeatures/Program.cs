4e41bb62bc7a96f27445c9285ba6fa754c6776b4
Souto, LA (Luis) <luis.souto@rabobank.com>
2024-09-17 12:03:49 +0200

    Initialization with [].

--- CSharpNewFeatures/Program.cs
+++ CSharpNewFeatures/Program.cs
@@ -12,3 +12,16 @@ MyCustomClass myClassNew = new();
 MyCustomClass myClassWithName = new("someName");
 
 #endregion var vs new
+
+#region Initialization with []
+
+List<double> emptyList = [];
+List<double> nonEmptyList = [2.1, 3.3, 34544354231.1];
+
+double[] emptyArray = [];
+double[] nonEmptyArray = [1, 3, 4, 0];
+
+Dictionary<string, double> emptyDict = [];
+Dictionary<string, double> nonEmptyDict = new() { ["red"] = 10, ["blue"] = 15, ["green"] = 20 };
+
+#endregion Initialization with []
